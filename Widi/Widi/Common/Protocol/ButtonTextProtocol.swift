b39b9dd0dfba936474c03c85306655fd1d0e25c6
AppleDeveloper <ikejhc159@gmail.com>
2025-06-01 14:13:07 +0900

    ♻️ [Refactor] Alert 4종류로 분리
    4종류에 따라 분리되어 표시될 수 있도록 지정

--- Widi/Widi/Common/Protocol/ButtonTextProtocol.swift
+++ Widi/Widi/Common/Protocol/ButtonTextProtocol.swift
@@ -10,9 +10,13 @@ import Foundation
 protocol ButtonTextProtocol {
     var deleteText: String { get }
     var cancelText: String { get }
+    var withdraw: String { get }
+    var delete: String { get }
 }
 
 extension ButtonTextProtocol {
     var deleteText: String { "삭제하기" }
     var cancelText: String { "돌아가기" }
+    var withdraw: String { "탈퇴하기" }
+    var delete: String { "삭제하기" }
 }
