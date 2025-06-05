dcd7f256c8c2f457e6a5d539fc0d242f8a7a313f
jeongminji <parapin2274@gmail.com>
2025-06-05 14:38:56 +0900

    ♻️ [Refactor] DIContainer추가, safeAreaPadding으로 변경, 액션시트에서 DropDown으로 이름 변경
    DIContainer추가, safeAreaPadding으로 변경, 액션시트에서 DropDown으로 이름 변경

--- Widi/Widi/Module/DetailFriends/Views/DetailFriendsView.swift
+++ Widi/Widi/Module/DetailFriends/Views/DetailFriendsView.swift
@@ -10,15 +10,17 @@ import SwiftUI
 struct DetailFriendsView: View {
     
     // MARK: - Properties
-
+    
     @Bindable private var viewModel: DetailFriendsViewModel
+    
+    @EnvironmentObject var container: DIContainer
     @State private var headerOffsets: (CGFloat, CGFloat) = (0, 0)
     @State private var diariesOffsets: [UUID: CGFloat] = [:]
-    @State private var isActionSheetPresented: Bool = false
+    @State private var isDropDownPresented: Bool = false
     
     private let naviHeight: CGFloat = 56
     private let headerHeight: CGFloat = 209
-
+    
     
     // MARK: - Init
     
@@ -43,7 +45,6 @@ struct DetailFriendsView: View {
             dropDownOverlay
         }
         .detailFriendViewBG()
-       
     }
 }
 
@@ -67,7 +68,7 @@ fileprivate extension DetailFriendsView {
                 switch icon {
                 case .contextMenu:
                     withAnimation(.easeInOut(duration: 0.2)) {
-                        isActionSheetPresented = true
+                        isDropDownPresented = true
                     }
                 default:
                     break
@@ -83,32 +84,37 @@ fileprivate extension DetailFriendsView {
         VStack {
             Spacer()
             
-            // TODO: - 일기 추가 화면 이동
-            DiariesAddButton(action: {})
-                .frame(width: 56)
-                .padding(.trailing, 20)
-                .padding(.bottom, 12)
+            DiariesAddButton(
+                action: {
+                    container.navigationRouter.push(to: .addDiaryView)
+                }
+            )
+            .frame(width: 56)
+            .safeAreaPadding(.trailing, 20)
+            .safeAreaPadding(.bottom, 12)
         }
     }
     
     /// 일기 검색, 친구 수정, 친구 삭제 액션시트
     @ViewBuilder
     var dropDownOverlay: some View {
-        if isActionSheetPresented {
+        if isDropDownPresented {
             Color.clear
                 .ignoresSafeArea()
                 .contentShape(Rectangle())
                 .onTapGesture {
                     withAnimation(.easeInOut(duration: 0.2)) {
-                        isActionSheetPresented = false
+                        isDropDownPresented = false
                     }
                 }
             
             FriendDropDown(onSelect: { selected in
                 switch selected {
                 case .search:
-                    // TODO: - 바텀시트 등장
-                    print("검색")
+                    // TODO: - 일기 검색화면 네비 연결
+                    withAnimation {
+                        isDropDownPresented = false
+                    }
                 case .edit:
                     // TODO: - 네비게이션 연결
                     print("편집")
@@ -117,7 +123,7 @@ fileprivate extension DetailFriendsView {
                     print("삭제")
                 }
             })
-            .padding(.horizontal, 22)
+            .safeAreaPadding(.horizontal, 16)
             .transition(.scale(scale: 0.7, anchor: .topTrailing).combined(with: .opacity))
         }
     }
@@ -197,6 +203,10 @@ fileprivate extension DetailFriendsView {
                     }
                 )
                 .frame(height: 171)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    container.navigationRouter.push(to: .detailDiaryView)
+                }
                 
                 if index < viewModel.diaries.count - 1 {
                     Divider()
@@ -255,41 +265,54 @@ fileprivate struct HeaderView: View {
     @Previewable @State var dummyDiaries: [DiaryResponse] = [
         DiaryResponse(
             id: UUID(),
-            title: "학식당에서",
-            content: "명상의 힘은 마음을 가라앉히고 내면의 평화를 찾는 방법을 제공합니다. 정기적인 명상은 스트레스를 줄이고 집중력을 향상시키는 방법입니다.",
-            pictures:  ["https://i.namu.wiki/i/4HF0qDNbaYaUTHCyJJTMPJ9ADmbXdc4C6ahEqIxURdzOeBZqIxzY69Xu9EbP3qlX-kCCunsBwAZpSvccoHLiFGcdpbHaeBz2QpFDzVrAoc6PFvj_ieSeVQwvn-gMKveZAj-EtVaxqdf7G6Q2zSXDnw.webp"],
-            diaryDate: "2025 / 05 / 24"
+            title: "여름 바다",
+            content: "뜨거운 여름날, 시원한 바다를 바라보며 친구들과 놀았다. 바람이 기분 좋았다.",
+            pictures: ["https://pimg.mk.co.kr/news/cms/202403/29/20240329_01110601000001_L01.jpg", "https://pimg.mk.co.kr/news/cms/202403/29/20240329_01110601000001_L01.jpg"],
+            diaryDate: "2025 / 06 / 01"
         ),
         DiaryResponse(
             id: UUID(),
-            title: "학식당에서",
-            content: "명상의 힘은 마음을 가라앉히고 내면의 평화를 찾는 방법을 제공합니다. 정기적인 명상은 스트레스를 줄이고 집중력을 향상시키는 방법입니다.",
-            pictures:  ["https://i.namu.wiki/i/4HF0qDNbaYaUTHCyJJTMPJ9ADmbXdc4C6ahEqIxURdzOeBZqIxzY69Xu9EbP3qlX-kCCunsBwAZpSvccoHLiFGcdpbHaeBz2QpFDzVrAoc6PFvj_ieSeVQwvn-gMKveZAj-EtVaxqdf7G6Q2zSXDnw.webp"],
-            diaryDate: "2025 / 05 / 24"
+            content: "노랗게 물든 단풍길을 걷다 보니 가을이 깊어감을 느꼈다. 사진도 많이 찍었다.",
+            pictures: [],
+            diaryDate: "2025 / 01 / 15"
         ),
         DiaryResponse(
             id: UUID(),
-            title: "학식당에서",
-            content: "명상의 힘은 마음을 가라앉히고 내면의 평화를 찾는 방법을 제공합니다. 정기적인 명상은 스트레스를 줄이고 집중력을 향상시키는 방법입니다.",
-            pictures:  ["https://i.namu.wiki/i/4HF0qDNbaYaUTHCyJJTMPJ9ADmbXdc4C6ahEqIxURdzOeBZqIxzY69Xu9EbP3qlX-kCCunsBwAZpSvccoHLiFGcdpbHaeBz2QpFDzVrAoc6PFvj_ieSeVQwvn-gMKveZAj-EtVaxqdf7G6Q2zSXDnw.webp"],
-            diaryDate: "2025 / 05 / 24"
+            content: "도서관에서 하루 종일 책을 읽었다. 오랜만에 조용한 시간을 보냈다.",
+            pictures: ["https://mblogthumb-phinf.pstatic.net/MjAyNDA4MDRfMjI5/MDAxNzIyNzU1NDE0MTQ5.gevr23_H7cZd_TFFvMwxxxknSY64mOvjRsBbNjwSopsg.57UoK7G4ioWfjIuEYDBQ0qmYnwd-hbBfETbTa13Y8tcg.JPEG/20230801%EF%BC%BF092949.jpg?type=w800"],
+            diaryDate: "2025 / 02 / 20"
         ),
         DiaryResponse(
             id: UUID(),
-            title: "학식당에서",
-            content: "명상의 힘은 마음을 가라앉히고 내면의 평화를 찾는 방법을 제공합니다. 정기적인 명상은 스트레스를 줄이고 집중력을 향상시키는 방법입니다.",
-            pictures:  ["https://i.namu.wiki/i/4HF0qDNbaYaUTHCyJJTMPJ9ADmbXdc4C6ahEqIxURdzOeBZqIxzY69Xu9EbP3qlX-kCCunsBwAZpSvccoHLiFGcdpbHaeBz2QpFDzVrAoc6PFvj_ieSeVQwvn-gMKveZAj-EtVaxqdf7G6Q2zSXDnw.webp"],
-            diaryDate: "2025 / 05 / 24"
+            title: "친구와 영화",
+            content: "친구와 최신 영화를 봤다. 재미있고 감동적인 장면이 많았다.",
+            pictures: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS59y8lxcgT1bpo-UFhKxUUp7hT8z7PUWr97A&s"],
+            diaryDate: "2025 / 03 / 12"
         ),
         DiaryResponse(
             id: UUID(),
-            title: "학식당에서",
-            content: "명상의 힘은 마음을 가라앉히고 내면의 평화를 찾는 방법을 제공합니다. 정기적인 명상은 스트레스를 줄이고 집중력을 향상시키는 방법입니다.",
-            pictures:  ["https://i.namu.wiki/i/4HF0qDNbaYaUTHCyJJTMPJ9ADmbXdc4C6ahEqIxURdzOeBZqIxzY69Xu9EbP3qlX-kCCunsBwAZpSvccoHLiFGcdpbHaeBz2QpFDzVrAoc6PFvj_ieSeVQwvn-gMKveZAj-EtVaxqdf7G6Q2zSXDnw.webp"],
-            diaryDate: "2025 / 05 / 24"
+            title: "봄꽃 축제",
+            content: "만개한 봄꽃들을 구경하러 갔다. 형형색색 꽃들이 아름다웠다.",
+            pictures: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQac9U1pW4QDEPBuVcBJTAEuPTQ-diXo5vEC6dsv6M-sUN_9srqggXHax2itKHC26IkKc8&usqp=CAU"],
+            diaryDate: "2025 / 10 / 05"
+        ),
+        DiaryResponse(
+            id: UUID(),
+            title: "비 오는 날",
+            content: "창밖으로 내리는 비를 바라보며 따뜻한 커피를 마셨다. 잔잔한 하루였다.",
+            pictures: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv2znysM8hscNWLP8pdjV0iEG_Nd7sox20DvoaPmV68a4iN8XWnbvqpqvIyxO0MkLeouA&usqp=CAU"],
+            diaryDate: "2025 / 04 / 18"
+        ),
+        DiaryResponse(
+            title: "겨울 산책",
+            content: "눈이 소복이 쌓인 길을 따라 조용히 산책을 했다. 고요하고 평화로운 느낌이었다.",
+            pictures: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8oKFPctzGt7mCmM06q7Qp8qM0dO3S9LjHruBsuvAtprnmBxeAXYZymZKp84qVxD1puLc&usqp=CAU"],
+            diaryDate: "2025 / 04 / 10"
         )
     ]
     
     let viewModel = DetailFriendsViewModel(friendData: dummyFriend, diaries: dummyDiaries)
+    
     DetailFriendsView(viewModel: viewModel)
+        .environmentObject(DIContainer())
 }
